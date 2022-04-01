824e1174293c602d7a5bb35d939aafcf52e75c7d
Simon Gaviria <simongaviria1@gmail.com>
2022-04-01 16:28:52 -0400

    fixed queries in the backend, added booking a meeting functionality

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -25,11 +25,11 @@ function createMeetingRoom(req, res) {
 }
 
 function getMeetingRoomById(req, res) {
-  console.log(`req.params`, req.params.id);
+  console.log(`req.params`, req.query.id);
   db.one(
     "SELECT * FROM meeting_rooms WHERE meeting_room_id=${meeting_room_id};",
     {
-      meeting_room_id: req.params.id,
+      meeting_room_id: req.query.id,
     }
   )
     .then((data) => {
@@ -45,7 +45,12 @@ function getMeetingRoomById(req, res) {
 }
 
 function getFutureBookings(req, res) {
-  db.any("SELECT * FROM bookings;", {})
+  db.any(
+    "SELECT * FROM bookings WHERE meeting_room_id=${meeting_room_id} and start_time >= now();",
+    {
+      meeting_room_id: req.query.id,
+    }
+  )
     .then((data) => {
       res.status(200).json({ meeting_rooms: data });
     })
@@ -69,25 +74,26 @@ function getBookings(req, res) {
     });
 }
 
-function getMeetingRoomBookings(req, res) {
-  console.log(`req.params`, req.params.id);
+function createBookingforMeetingRoom(req, res) {
+  console.log(`req.query`, req.query);
   db.any(
-    "select * from bookings where meeting_room_id=3 and start_time >= now();",
+    "INSERT INTO bookings (booking_id, meeting_room_id, start_time, end_time) VALUES(12345, 2, '20250618 10:34:09 AM', '20250618 10:34:09 AM');",
     {
-      meeting_room_id: req.params.id,
+      booking_id: req.params.booking_id,
+      meeting_room_id: req.params.meeting_room_id,
+      start_time: req.params.start_time,
+      end_time: req.params.end_time,
+      participants: req.params.participants,
     }
   )
     .then((data) => {
       res.status(200).json({
         status: "success",
-        data: data,
-        message: "Fetched meeting room",
+        message: "Room Booked",
       });
     })
     .catch((err) => {
-      res
-        .status(500)
-        .json({ message: `FAILED: Meeting room bookings does not exisit` });
+      res.status(500).json({ message: `FAILED: Could not book room` });
     });
 }
 
@@ -130,6 +136,6 @@ module.exports = {
   getFutureBookings,
   getBookings,
   getBookingById,
-  getMeetingRoomBookings,
+  createBookingforMeetingRoom,
   deleteBooking,
 };
